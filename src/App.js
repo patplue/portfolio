aa27edefe93ac5ccf3b10e26b85f757b879e18e2
Patrick Plue <pluepatrick@gmail.com>
2020-09-28 16:13:32 +0200

    init

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React nooooow
+          Learn React
         </a>
       </header>
     </div>
